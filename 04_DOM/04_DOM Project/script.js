d192e92bbf69448b9de17b5d7c302e7122085aa4
krutheesh <krutheeshsiripuram@gmail.com>
2023-03-02 22:01:16 +0530

    dom4_project

---
+++ 04_DOM/04_DOM Project/script.js
@@ -0,0 +1,20 @@
+const col = document.querySelectorAll(".clash-card__unit-stats");
+col[0].style.backgroundColor = "#ffa64d";
+col[0].style.color = "#FFFFFF";
+
+col[1].style.backgroundColor = "#ff4d88";
+col[1].style.color = "#FFFFFF";
+
+col[2].style.backgroundColor = "#ff8c1a";
+col[2].style.color = "#FFFFFF";
+
+col[3].style.backgroundColor = "#66cc00";
+col[3].style.color = "#FFFFFF";
+
+col[4].style.backgroundColor = "#00aaff";
+col[4].style.color = "#FFFFFF";
+
+const chilColor = document.querySelectorAll(".no-border");
+for(let i=0 ;i<chilColor.length ;i++){
+  chilColor[i].style.color = "#FFFFFF"
+}
